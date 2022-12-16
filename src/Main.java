986a4c404280d4164f4643add08b949e55cd31b2
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-16 23:56:11 +0900

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        isLeapYear(2008);
+    }
 
+    public static void isLeapYear(int year) {
+        System.out.println("Задача 1");
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
