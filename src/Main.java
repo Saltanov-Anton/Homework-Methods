a2f2b9a11e83e2dfb7827ab53c13a923c13e00dc
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-17 00:06:35 +0900

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         isLeapYear(2008);
+        whichVersionApp(0, 2022);
     }
 
     public static void isLeapYear(int year) {
@@ -9,6 +12,20 @@ public class Main {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
+        }
+    }
+
+    public static void whichVersionApp(int os, int deviceYear) {
+        System.out.println("Задача 2");
+        int currentYear = LocalDate.now().getYear();
+        if (os == 0 && deviceYear >= currentYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (os == 0 && deviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (os == 1 && deviceYear >= currentYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (os == 1 && deviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
     }
 }
