03086f61b02dfb20059a3207e10238550a01599c
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-17 00:15:36 +0900

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         isLeapYear(2008);
         whichVersionApp(0, 2022);
+        System.out.println("Потребуется дней " + calculateDelivery(5));
     }
 
     public static void isLeapYear(int year) {
@@ -27,5 +28,18 @@ public class Main {
         } else if (os == 1 && deviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
+    }
+
+    public static int calculateDelivery(int distance) {
+        System.out.println("Задача 3");
+        int days = 0;
+        if (distance <= 20) {
+            days = 1;
+        } else if (distance > 20 && distance <= 60) {
+            days = 2;
+        } else if (distance > 60 && distance <= 100) {
+            days = 3;
+        }
+        return days;
     }
 }
